195c2f5daeadc8374fda7510f4b424bc108ec8e2
DenilsonCunha <denilsonluciano6@gmail.com>
2022-02-03 19:13:13 -0300

    configurando firebase

--- src/pages/ChatRoom/index.js
+++ src/pages/ChatRoom/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import auth from '@react-native-firebase/auth';
+
 export default function ChatRoom() {
   return (
     <View style={styles.container}>
