e2f7bd65c07d1afdde06f61836bf4decb925a0a4
DenilsonCunha <denilsonluciano6@gmail.com>
2022-02-03 14:07:09 -0300

    atualizando rotas

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,20 +1,28 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import SignIn from '../pages/SignIn';
+import ChatRoom from '../pages/ChatRoom';
 
 const AppStack = createNativeStackNavigator();
 
 function AppRoutes() {
   return (
-    <AppStack.Navigator>
+    <AppStack.Navigator initialRouteName='ChatRoom'>
       <AppStack.Screen
         name="SignIn"
         component={SignIn}
         options={{
           title: 'Faça o login',
         }}
+      />
+
+      <AppStack.Screen
+        name="ChatRoom"
+        component={ChatRoom}
+          options={{
+            headerShown:false
+          }}
       />
     </AppStack.Navigator>
   );
